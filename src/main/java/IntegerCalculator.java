c6be1470f4abe7eec8065c4e2521c429cab2f370
Yevhen Bilevych <Yevhen.Bilevych@eleks.com>
2018-03-26 22:43:24 +0300

    Initial commit

---
+++ src/main/java/IntegerCalculator.java
@@ -0,0 +1,6 @@
+
+public class IntegerCalculator {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/TestIntegerCalculator.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Yevhen.Bilevych on 26.03.2018.
+ */
+public class TestIntegerCalculator {
+    @Test
+    public void testMultiplication() {
+        Assert.assertEquals(4, IntegerCalculator.multiply(2, 2));
+    }
+
+}
