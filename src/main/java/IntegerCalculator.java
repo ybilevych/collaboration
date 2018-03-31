a4ffde65d75d1321a18d3027f288a900a74576ae
Yevhen Bilevych <ybilevych@gmail.com>
2018-03-31 18:16:54 +0300

    Update IntegerCalculator.java

--- src/main/java/IntegerCalculator.java
+++ src/main/java/IntegerCalculator.java
@@ -7,4 +7,7 @@ public class IntegerCalculator {
     public static int divide(int a, int b) {
         return a / b;
     }
+    public static int summ(int a, int b) {
+        return a + b;
+    }
 }
