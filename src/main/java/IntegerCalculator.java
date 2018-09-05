46a00b34efe5ba7d9be9f0cfc33a3294e5517f04
Yevhen Bilevych <ybilevych@gmail.com>
2018-09-05 19:02:29 +0300

    Update IntegerCalculator.java
    scm poll test

--- src/main/java/IntegerCalculator.java
+++ src/main/java/IntegerCalculator.java
@@ -1,7 +1,7 @@
 
 public class IntegerCalculator {
     public static int multiply(int a, int b) {
-        return a * b;
+        return a * b + 1;
     }
 
     public static int divide(int a, int b) {
